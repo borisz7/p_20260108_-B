0d6fede9f3f8ab9161abd777a626654e6f629b3d
borisz7 <borisz.acs@gmail.com>
2026-01-08 13:44:33 +0100

    erw

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace p_20260108_ÁB
 {
@@ -27,10 +29,21 @@ namespace p_20260108_ÁB
         public List<string> Tasklist = new List<string>();
         private void rogzit_Click(object sender, RoutedEventArgs e)
         {
+            string fnv = "";
             Tasklist.Add(feladat.Text);
             //feladatok.Items.Add(rogzit.Content);
             //feladatok.ItemsSource = feladatok.Items;
             feladatok.ItemsSource = Tasklist;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+            FileStream f = new FileStream(saveFileDialog.FileName, FileMode.Create);
+            StreamWriter ir = new StreamWriter(f);
+            foreach (string item in Tasklist)
+            {
+                ir.WriteLine(item);
+            }
+            ir.Close();
+            f.Close();
         }
 
         private void feladat_TextChanged(object sender, TextChangedEventArgs e)
