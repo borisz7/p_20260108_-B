2d55ddde4110f46eda737d898022061aca0352e4
borisz7 <borisz.acs@gmail.com>
2026-01-08 13:13:42 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace p_20260108_ÁB
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace p_20260108_ÁB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public List<string> Tasklist = new List<string>();
+        private void rogzit_Click(object sender, RoutedEventArgs e)
+        {
+            Tasklist.Add(feladat.Text);
+            //feladatok.Items.Add(rogzit.Content);
+            //feladatok.ItemsSource = feladatok.Items;
+            feladatok.ItemsSource = Tasklist;
+        }
+
+        private void feladat_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
